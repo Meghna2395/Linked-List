9a9f598c831d4cc9fe29bad35bb72f400df7fcd1
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-18 16:55:13 +0530

    Add files via upload

---
+++ List.java
@@ -0,0 +1,15 @@
+package com.bridzelabz.linkedlist;
+
+public class List{ 
+	  public static void main(String[] args) {
+	    LinkedList List = new LinkedList();
+
+	    //Add four elements in the list.
+	    List.push(56);
+	    List.push(30);
+	    List.push(70);
+	    List.PrintList(); 
+
+
+	  }
+	}
